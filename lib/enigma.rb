6c227617b82b8950c36c2ae113900ccdb5924e23
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-11 13:03:34 -0700

    Feat: encrypt message_key defaults to random key

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,7 +2,7 @@ require 'date'
 require 'pry'
 
 class Enigma
-  def encrypt(message, message_key, message_date = Date.today.strftime("%m%d%y"))
+  def encrypt(message, message_key = rand.to_s[2..6], message_date = Date.today.strftime("%m%d%y"))
     # binding.pry
     hash = {
       :encryption => message,
