9e663eef082a495a8ac06a32838e94e20bbeb52d
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-11 11:44:42 -0700

    Feat, turned keys into symbols

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,9 +4,9 @@ require 'pry'
 class Enigma
   def encrypt(message, message_key, message_date)
     hash = {
-      encryption: message,
-      key: message_key,
-      date: message_date
+      :encryption => message,
+      :key => message_key,
+      :date => message_date
     }
   end
 end
