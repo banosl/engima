4c808b290963af7f7b9e81edaa46b7bd2df0cc09
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-11 11:15:26 -0700

    Feat: #encrypt defined

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,12 @@
 require 'date'
+require 'pry'
 
 class Enigma
-    
+  def encrypt(message, message_key, message_date)
+    hash = {
+      encryption: message,
+      key: message_key,
+      date: message_date
+    }
+  end
 end
