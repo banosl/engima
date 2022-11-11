42829efb02f8f58b59f5f60b5aadf9a0ee349894
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-11 11:52:54 -0700

    Test: when no date is given the default is today's

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -14,12 +14,15 @@ RSpec.describe Enigma do
       expect(@enigma).to be_instance_of(Enigma)
     end
 
-    it 'encrypt takes three arguments and makes a hash' do
+    it '#encrypt takes three arguments and makes a hash' do
       expect(@enigma.encrypt('hello world', '02715', '040895')).to be_instance_of(Hash)
       expect(@enigma.encrypt('hello world', '02715', '040895')[:encryption]).to eq('hello world')
       expect(@enigma.encrypt('hello world', '02715', '040895')[:key]).to eq('02715')
       expect(@enigma.encrypt('hello world', '02715', '040895')[:date]).to eq('040895')
-      
+    end
+
+    it '#encrypt when no date is given, it defaults the date value to todays date' do
+      expect(expect(@enigma.encrypt('hello world', '02715'))[:date]).to eq('111122')  
     end
   end
 end
