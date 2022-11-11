79a175c0bdffe35da2eeff44d54f701416ee9835
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-11 13:47:35 -0700

    Feat: #build_key

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -10,4 +10,14 @@ class Enigma
       :date => message_date
     }
   end
+
+  def build_key(message_key = rand.to_s[2..6])
+    # binding.pry
+    abcd_keys = {
+                :a =>  "#{message_key[0]}#{message_key[1]}",
+                :b =>  "#{message_key[1]}#{message_key[2]}",
+                :c =>  "#{message_key[2]}#{message_key[3]}",
+                :d =>  "#{message_key[3]}#{message_key[4]}"
+    }
+  end
 end
