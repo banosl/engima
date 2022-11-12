49831524f4700be46aab5732fbb78a6bb71c36d8
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-11 18:54:09 -0700

    Test: for shifting just shift A in message

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -61,5 +61,15 @@ RSpec.describe Enigma do
 
       expect(@enigma.key_plus_offset(keys, offsets)).to be_instance_of(Hash)
     end
+
+    it '#shift_letter can change the first character to another' do
+        key = '02715'
+      offset = '040895'
+      keys = @enigma.build_key(key)
+      offsets = @enigma.build_offset(offset)
+    #   binding.pry
+        # expect(@enigma.shift_letter("hello")).to eq("kello")
+        expect(@enigma.shift_letter("hello world")).to eq('kellr would')
+    end
   end
 end
