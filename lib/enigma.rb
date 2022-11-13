218d7f2d8b3b8df7c786df0f42abae1d276ad235
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-13 16:08:04 -0700

    Feat: completed #encrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,12 +3,7 @@ require 'pry'
 
 class Enigma
   def encrypt(message, message_key = rand.to_s[2..6], message_date = Date.today.strftime('%m%d%y'))
-    # binding.pry
-    # keys = build_key(message_key)
-    # offsets = build_offset(message_date)
-    # # binding.pry
-    # key_plus_offset(keys, offsets)
-# binding.pry
+    
     shift_letter(message, message_key, message_date)
 
     hash = {
@@ -19,7 +14,6 @@ class Enigma
   end
 
   def build_key(message_key)
-    # binding.pry
     abcd_keys = {
       a: "#{message_key[0]}#{message_key[1]}",
       b: "#{message_key[1]}#{message_key[2]}",
@@ -30,7 +24,6 @@ class Enigma
 
   def build_offset(message_date)
     squared = (message_date.to_i**2).to_s
-    # binding.pry
     abcd_offset = {
       a: "#{squared[-4]}",
       b: "#{squared[-3]}",
@@ -40,24 +33,21 @@ class Enigma
   end
 
   def key_plus_offset(keys, offsets)
-    # binding.pry
     sum = {
       a: keys[:a].to_i + offsets[:a].to_i,
       b: keys[:b].to_i + offsets[:b].to_i,
       c: keys[:c].to_i + offsets[:c].to_i,
       d: keys[:d].to_i + offsets[:d].to_i
     }
-    # binding.pry
   end
 
   def shift_letter(message, message_key, message_date)
-    # binding.pry
     keys = build_key(message_key)
     offsets = build_offset(message_date)
     shift = key_plus_offset(keys, offsets)
     message.downcase!
     length = message.length
-# binding.pry
+    
     # A shift
     alphabet = ('a'..'z').to_a << ' '
     count = 0
@@ -67,7 +57,6 @@ class Enigma
       else
         a_letter = alphabet.rotate!(alphabet.index(message[count]))
         message.slice!(count)
-        # binding.pry
         message.insert(count, a_letter.rotate(shift[:a])[0])
         count += 4
       end
@@ -83,7 +72,7 @@ class Enigma
       else
         b_letter = alphabet.rotate!(alphabet.index(message[count]))
         message.slice!(count)
-        message.insert(count, b_letter.rotate(27)[0])
+        message.insert(count, b_letter.rotate(shift[:b])[0])
         count += 4
       end
       break if count >= length
@@ -98,7 +87,7 @@ class Enigma
       else
         c_letter = alphabet.rotate!(alphabet.index(message[count]))
         message.slice!(count)
-        message.insert(count, c_letter.rotate(73)[0])
+        message.insert(count, c_letter.rotate(shift[:c])[0])
         count += 4
       end
       break if count >= length
@@ -114,7 +103,7 @@ class Enigma
 
         d_letter = alphabet.rotate!(alphabet.index(message[count]))
         message.slice!(count)
-        message.insert(count, d_letter.rotate(20)[0])
+        message.insert(count, d_letter.rotate(shift[:d])[0])
         count += 4
       end
       break if count >= length
