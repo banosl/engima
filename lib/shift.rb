1686f089699416409d8a80416d0a03956de221b0
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-14 19:22:02 -0700

    Refactor shift_loop

--- lib/shift.rb
+++ lib/shift.rb
@@ -33,65 +33,23 @@ module Shift
     alphabet = ('a'..'z').to_a << ' '
     count = 1
     shift_loop(alphabet, count, message, letter_shift, length, encrypt)
-
-    # loop do
-    #   if !alphabet.include?(message[count])
-    #     count += 4
-    #   else
-    #     b_letter = alphabet.rotate!(alphabet.index(message[count]))
-    #     message.slice!(count)
-    #     if encrypt == true
-    #       message.insert(count, b_letter.rotate(shift[:b])[0])
-    #     else
-    #       message.insert(count, b_letter.rotate(-shift[:b])[0])
-    #     end
-    #     count += 4
-    #   end
-    #   break if count >= length
-    # end
   end
 
   def c_shift(message, shift, encrypt)
     length = message.length
     message.downcase!
+    letter_shift = shift[:c]
     alphabet = ('a'..'z').to_a << ' '
     count = 2
-    loop do
-      if !alphabet.include?(message[count])
-        count += 4
-      else
-        c_letter = alphabet.rotate!(alphabet.index(message[count]))
-        message.slice!(count)
-        if encrypt == true
-          message.insert(count, c_letter.rotate(shift[:c])[0])
-        else
-          message.insert(count, c_letter.rotate(-shift[:c])[0])
-        end
-        count += 4
-      end
-      break if count >= length
-    end
+    shift_loop(alphabet, count, message, letter_shift, length, encrypt)
   end
 
   def d_shift(message, shift, encrypt)
     length = message.length
     message.downcase!
+    letter_shift = shift[:d]
     alphabet = ('a'..'z').to_a << ' '
     count = 3
-    loop do
-      if !alphabet.include?(message[count])
-        count += 4
-      else
-        d_letter = alphabet.rotate!(alphabet.index(message[count]))
-        message.slice!(count)
-        if encrypt == true
-          message.insert(count, d_letter.rotate(shift[:d])[0])
-        else
-          message.insert(count, d_letter.rotate(-shift[:d])[0])
-        end
-        count += 4
-      end
-      break if count >= length
-    end
+    shift_loop(alphabet, count, message, letter_shift, length, encrypt)
   end
 end
