dfed0cee66f6b539577c70f0e9423c67f81a1497
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-15 11:23:52 -0700

    removed comments and tried different message for decryption

--- lib/crack.rb
+++ lib/crack.rb
@@ -4,13 +4,11 @@ require 'pry'
 enigma = Enigma.new
 
 message = File.open(ARGV[0], 'r')
-# binding.pry
 encryption_read = message.read.split(',')[0]
 message.close
-# binding.pry
 decryption = enigma.crack(encryption_read, ARGV[2])
-binding.pry
-puts "Created 'cracked.txt' with the cracked key and date #{ARGV[2]}"
+
+puts "Created 'cracked.txt' with the cracked key #{decryption[:key]} and date #{decryption[:date]}"
 
 decrypted_message = File.open(ARGV[1], 'w')
 decrypted_message.write(decryption[:decryption], ',', decryption[:key], ',', decryption[:date])
