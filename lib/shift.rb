89a6bb8d6f0af3445a2473dd78f169eb492554c3
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-14 19:20:06 -0700

    Refactoring the loops in the shifts

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,20 +1,15 @@
 module Shift
-
-  def a_shift(message, shift, encrypt)
-    length = message.length
-    message.downcase!
-    alphabet = ('a'..'z').to_a << ' '
-    count = 0
+  def shift_loop(alphabet, count, message, letter_shift, length, encrypt)
     loop do
       if !alphabet.include?(message[count])
         count += 4
       else
-        a_letter = alphabet.rotate!(alphabet.index(message[count]))
+        letter = alphabet.rotate!(alphabet.index(message[count]))
         message.slice!(count)
         if encrypt == true
-          message.insert(count, a_letter.rotate(shift[:a])[0])
+          message.insert(count, letter.rotate(letter_shift).first)
         else
-          message.insert(count, a_letter.rotate(-shift[:a])[0])
+          message.insert(count, letter.rotate(-letter_shift).first)
         end
         count += 4
       end
@@ -22,26 +17,38 @@ module Shift
     end
   end
 
+  def a_shift(message, shift, encrypt)
+    length = message.length
+    message.downcase!
+    letter_shift = shift[:a]
+    alphabet = ('a'..'z').to_a << ' '
+    count = 0
+    shift_loop(alphabet, count, message, letter_shift, length, encrypt)
+  end
+
   def b_shift(message, shift, encrypt)
     length = message.length
     message.downcase!
+    letter_shift = shift[:b]
     alphabet = ('a'..'z').to_a << ' '
     count = 1
-    loop do
-      if !alphabet.include?(message[count])
-        count += 4
-      else
-        b_letter = alphabet.rotate!(alphabet.index(message[count]))
-        message.slice!(count)
-        if encrypt == true
-          message.insert(count, b_letter.rotate(shift[:b])[0])
-        else
-          message.insert(count, b_letter.rotate(-shift[:b])[0])
-        end
-        count += 4
-      end
-      break if count >= length
-    end
+    shift_loop(alphabet, count, message, letter_shift, length, encrypt)
+
+    # loop do
+    #   if !alphabet.include?(message[count])
+    #     count += 4
+    #   else
+    #     b_letter = alphabet.rotate!(alphabet.index(message[count]))
+    #     message.slice!(count)
+    #     if encrypt == true
+    #       message.insert(count, b_letter.rotate(shift[:b])[0])
+    #     else
+    #       message.insert(count, b_letter.rotate(-shift[:b])[0])
+    #     end
+    #     count += 4
+    #   end
+    #   break if count >= length
+    # end
   end
 
   def c_shift(message, shift, encrypt)
